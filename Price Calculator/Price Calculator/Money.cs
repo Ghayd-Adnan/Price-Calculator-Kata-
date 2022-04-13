583b147ebfde22ec7dc5305fcbb2f335a4ec2a68
Ghyad Adnan <ghayd.adnan2311@gmail.com>
2022-04-13 17:39:52 +0300

    Final version of Task 3 Price Calculator Code

---
+++ Price Calculator/Price Calculator/Money.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Price_Calculator
+{
+    public class Money
+    {
+        public double amount { get; set; }
+       public string currency { get; set; }
+
+        public Money(double amount,string currency) { this.amount = amount; this.currency = currency; }
+    }
+}
---
+++ Price Calculator/Price Calculator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Price_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string cap = "30%";
+            Product product1 = new Product("Milk",12345,new Money(20.25,"USD"));
+            var tax_dis = new Dictionary<string, int>();
+            tax_dis.Add("Tax",21);
+            tax_dis.Add("Discount",15);
+            product1.PriceDetailsReport(tax_dis,1,cap);// 0 Additive discount ,1 Multiplicative
+        }
+    }
+}
